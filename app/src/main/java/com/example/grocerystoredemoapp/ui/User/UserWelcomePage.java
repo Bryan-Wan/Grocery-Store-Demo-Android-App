dfb17bd227997a81d16eab59e496642805456dd6
Brandon Duan <bizzarebeta@gmail.com>
2021-11-26 16:49:51 -0500

    adding log out button

---
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserWelcomePage.java
@@ -0,0 +1,15 @@
+package com.example.grocerystoredemoapp.ui.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserWelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
