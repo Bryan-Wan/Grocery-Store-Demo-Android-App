6acf21316a1f13d293eb9e6997a71efefc8e5b22
shzehi <shadmehr81522@gmail.com>
2021-11-27 20:32:46 -0500

    New register page

---
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/login/LoginRegister.java
@@ -0,0 +1,16 @@
+package com.example.grocerystoredemoapp.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.grocerystoredemoapp.R;
+
+public class LoginRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+}
