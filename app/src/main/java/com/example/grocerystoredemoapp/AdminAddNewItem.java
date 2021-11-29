623fcabf1331ccafec95abec3c84e39f16dd1e1f
Mehrdad Ghannad <85261795+MehrdadQ@users.noreply.github.com>
2021-11-28 20:24:51 -0500

    added "My products" and "Add new item" pages

---
+++ app/src/main/java/com/example/grocerystoredemoapp/AdminAddNewItem.java
@@ -0,0 +1,14 @@
+package com.example.grocerystoredemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminAddNewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_new_item);
+    }
+}
---
+++ app/src/main/java/com/example/grocerystoredemoapp/AdminMyProducts.java
@@ -0,0 +1,14 @@
+package com.example.grocerystoredemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminMyProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_my_products);
+    }
+}
