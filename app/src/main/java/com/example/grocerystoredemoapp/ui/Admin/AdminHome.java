fccbad6a83401049303fe6387850e6de049f6807
Bryan-Wan <84968862+Bryan-Wan@users.noreply.github.com>
2021-11-29 02:46:47 -0500

    Added admin/user Pages
    Check Figma for completed Pages
    Reorganized String xml
    Added String
    Added missing .java files for some activities

--- app/src/main/java/com/example/grocerystoredemoapp/ui/Admin/AdminHome.java
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/Admin/AdminHome.java
@@ -1,14 +1,16 @@
-package com.example.grocerystoredemoapp;
+package com.example.grocerystoredemoapp.ui.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AdminAddNewItem extends AppCompatActivity {
+import com.example.grocerystoredemoapp.R;
+
+public class AdminHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_add_new_item);
+        setContentView(R.layout.activity_admin_home);
     }
 }
