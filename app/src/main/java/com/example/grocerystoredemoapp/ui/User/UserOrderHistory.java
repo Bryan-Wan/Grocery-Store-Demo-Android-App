b2a57cfad24e42cb3ea97f6abb238594d51d821c
Brandon Duan <94653916+BrandonDuan2021@users.noreply.github.com>
2021-11-29 16:30:15 -0500

    Have issue with the current version

---
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserOrderHistory.java
@@ -0,0 +1,12 @@
+package com.example.grocerystoredemoapp.ui.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserOrderHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
