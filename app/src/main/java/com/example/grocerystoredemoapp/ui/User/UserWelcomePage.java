6548225de6b735305ada310778b0d80bc654d0d7
Bryan-Wan <84968862+Bryan-Wan@users.noreply.github.com>
2021-11-29 19:59:05 -0500

    Revert "Merge pull request #12 from Bryan-Wan/Brandon"
    This reverts commit 4f65df151bfeb825bd53bc863a1f36561028bedc, reversing
    changes made to 6e84b69f3422c37ded197823a4787bdb53ff8072.

--- app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserWelcomePage.java
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserWelcomePage.java
@@ -2,43 +2,14 @@ package com.example.grocerystoredemoapp.ui.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.example.grocerystoredemoapp.R;
 
 public class UserWelcomePage extends AppCompatActivity {
-    Button placeNewOrder;
-    Button orderHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+    }
 
-        placeNewOrder = findViewById(R.id.placeOrder);
-        orderHistory = findViewById(R.id.orderHistory);
 
-        placeNewOrder.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View view) {
-                                                 Intent i = new Intent(UserWelcomePage.this, UserNewOrderPage.class);
-                                                 startActivity(i);
-
-                                             }
-                                         }
-        );
-
-        orderHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(UserWelcomePage.this, UserOrderHistory.class);
-                startActivity(i);
-            }
-        }
-        );
-
-}
 }
