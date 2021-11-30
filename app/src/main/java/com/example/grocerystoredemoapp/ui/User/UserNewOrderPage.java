712d160014a20b01d8ef54bc0cc3b02cab892ebc
Brandon Duan <bizzarebeta@gmail.com>
2021-11-30 14:24:51 -0500

    commit changes
    This reverts commit 4f65df151bfeb825bd53bc863a1f36561028bedc, reversing
    changes made to 6e84b69f3422c37ded197823a4787bdb53ff8072.

---
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserNewOrderPage.java
@@ -0,0 +1,16 @@
+package com.example.grocerystoredemoapp.ui.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.grocerystoredemoapp.R;
+
+public class UserNewOrderPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_new_order_page);
+    }
+}
