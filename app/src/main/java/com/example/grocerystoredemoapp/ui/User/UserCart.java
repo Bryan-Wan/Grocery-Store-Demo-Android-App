2856dbfeb091d988601c657536006f7604ae1b8b
Mehrdad Ghannad <85261795+MehrdadQ@users.noreply.github.com>
2021-12-02 15:31:29 -0500

    Added navigation between cart and user confirmation pages
    see previous commits for other changes

--- app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserCart.java
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserCart.java
@@ -2,6 +2,7 @@ package com.example.grocerystoredemoapp.ui.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +26,12 @@ public class UserCart extends AppCompatActivity {
         View view = getLayoutInflater().inflate(R.layout.activity_user_product_added_view, null, false);
         scrollingLayout.addView(view);
 
-
+        purchase.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(UserCart.this, UserConfirmationPage.class));
+            }
+        });
 
 
 
