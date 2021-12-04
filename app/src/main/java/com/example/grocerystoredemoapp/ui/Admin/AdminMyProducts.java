dbe51753b08c56bcbcdf5f797224c05d8815a87b
Bryan-Wan <84968862+Bryan-Wan@users.noreply.github.com>
2021-12-04 00:54:44 -0500

    AdminMyProduct now displays product info

--- app/src/main/java/com/example/grocerystoredemoapp/ui/Admin/AdminMyProducts.java
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/Admin/AdminMyProducts.java
@@ -14,6 +14,8 @@ import android.widget.ListView;
 import com.example.grocerystoredemoapp.R;
 
 import com.example.grocerystoredemoapp.R;
+import com.example.grocerystoredemoapp.data.model.Product;
+import com.example.grocerystoredemoapp.data.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -43,15 +45,37 @@ public class AdminMyProducts extends AppCompatActivity {
             }
         });
         DatabaseReference storeRef = FirebaseDatabase.getInstance().getReference("StoreData").child(currentFirebaseUser.getUid()).child("products");
+        DatabaseReference productRef = FirebaseDatabase.getInstance().getReference("Product");
 
         storeRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 ArrayList<String> newId = new ArrayList<>();
-                    if (snapshot.getValue() instanceof ArrayList && ((ArrayList) snapshot.getValue()).size() > 0 && ((ArrayList) snapshot.getValue()).get(0) instanceof String) {
+                ArrayList<String> productInfo = new ArrayList<>();
+
+                if (snapshot.getValue() instanceof ArrayList && ((ArrayList) snapshot.getValue()).size() > 0 && ((ArrayList) snapshot.getValue()).get(0) instanceof String) {
                         newId = (ArrayList<String>) snapshot.getValue();
-                        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,newId);
-                        mListView.setAdapter(adapter);
+
+                        ArrayList<String> finalNewId = newId;
+                        productRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                productRef.removeEventListener(this);
+                                for (DataSnapshot ds : snapshot.getChildren()) {
+                                    Product product = ds.getValue(Product.class);
+                                    if(finalNewId.contains(ds.getKey())){
+                                        productInfo.add(product.getName() + " " + product.getBrand() + " $" + product.getPrice());
+                                    }
+                                }
+                                ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,productInfo);
+                                mListView.setAdapter(adapter);
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
                     }
                 }
 
