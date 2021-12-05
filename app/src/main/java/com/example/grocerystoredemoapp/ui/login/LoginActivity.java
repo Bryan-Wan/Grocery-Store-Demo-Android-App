7441524c9b064a96ebf0f2ea8da1f325db78c079
shzehi <shadmehr81522@gmail.com>
2021-12-04 23:01:10 -0500

    updated user history
    + changes in string
    + removed test buttons in login

--- app/src/main/java/com/example/grocerystoredemoapp/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.grocerystoredemoapp.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -14,9 +15,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,18 +27,26 @@ import android.widget.Toast;
 
 import com.example.grocerystoredemoapp.R;
 import com.example.grocerystoredemoapp.data.model.LoggedInUser;
-import com.example.grocerystoredemoapp.databinding.ActivityLoginBinding;
+import com.example.grocerystoredemoapp.data.model.StoreData;
+import com.example.grocerystoredemoapp.data.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import com.example.grocerystoredemoapp.ui.Admin.AdminHome;
 import com.example.grocerystoredemoapp.ui.User.UserHome;
 import com.example.grocerystoredemoapp.ui.User.UserProductPage;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
-    private ActivityLoginBinding binding;
 
     private FirebaseAuth mAuth;
 
@@ -45,16 +56,14 @@ public class LoginActivity extends AppCompatActivity {
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-        
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_login);
 
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = binding.username;
-        final EditText passwordEditText = binding.password;
-        final Button loginButton = binding.loginBtn;
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.loginBtn);
 
         // Handle login input errors
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -140,32 +149,34 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-        // Go to Admin Home Page with TEST Button
-        Button adminTestBtn = (Button) findViewById(R.id.testLoginAsAdmin);
-        adminTestBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startHomePage(true);
-            }
-        });
-
-        // Go to User Home Page with TEST Button
-        Button userTestBtn = (Button) findViewById(R.id.testLoginAsUser);
-        userTestBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startHomePage(false);
-            }
-        });
-
     }
 
     @Override
     public void onStart() {
+        String userID;
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            // TODO: Call function that checks user type and switches to appropriate page
+            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Users");
+
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for(DataSnapshot ds: snapshot.getChildren()){
+                        User user = ds.getValue(User.class);
+                        if(ds.getKey().equals(currentUser.getUid())){
+                            if (user != null) {
+                                startHomePage(user.isAdmin());
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
         }
     }
 
@@ -173,6 +184,7 @@ public class LoginActivity extends AppCompatActivity {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        // TODO: THIS NEEDS TO BE CHANGED ALWAYS GOES TO ADMIN
         startHomePage(model.isAdmin());
     }
 
@@ -183,10 +195,10 @@ public class LoginActivity extends AppCompatActivity {
     private void startHomePage(boolean isAdmin) {
         if (isAdmin) {
             // Admin/store owner log in
-            startActivity(new Intent(LoginActivity.this, UserHome.class));
+            startActivity(new Intent(LoginActivity.this, AdminHome.class));
         } else {
             // User/shopper log in
-            startActivity(new Intent(LoginActivity.this, AdminHome.class));
+            startActivity(new Intent(LoginActivity.this, UserHome.class));
         }
         finish(); // Prevent going back to the login page when pressing back
     }
