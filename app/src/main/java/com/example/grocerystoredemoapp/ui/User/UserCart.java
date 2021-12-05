db7fba0fd3a6dd93962736a5cd6891a9a4d82844
Mehrdad Ghannad <85261795+MehrdadQ@users.noreply.github.com>
2021-12-05 15:02:04 -0500

    Cart now shows products in orders from user
    - also made small changes to layout and design of displayed products in cart

--- app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserCart.java
+++ app/src/main/java/com/example/grocerystoredemoapp/ui/User/UserCart.java
@@ -1,5 +1,6 @@
 package com.example.grocerystoredemoapp.ui.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,22 +11,76 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.grocerystoredemoapp.R;
-import com.example.grocerystoredemoapp.data.model.OrderData;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class UserCart extends AppCompatActivity {
     LinearLayout scrollingLayout;
     Button purchase;
-
+    static Integer nameID = 1111;
+    static Integer brandID = 2222;
+    static Integer priceID = 3333;
+    static Integer quantityID = 4444;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_cart);
 
-        scrollingLayout = findViewById(R.id.userCartScrollLayout);
-        purchase = findViewById(R.id.purchaseBtn);
-        addProduct();
+        DatabaseReference orders = FirebaseDatabase.getInstance().getReference().child("Order");
+        DatabaseReference products = FirebaseDatabase.getInstance().getReference().child("Product");
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        orders.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot sp : snapshot.getChildren()) {
+                    if (sp.child("byUser").exists() && sp.child("confirmOrder").getValue().toString().equals("false")) {
+                        if (currentUser.getUid().toString().equals(sp.child("byUser").getValue().toString())) {
+                            for (DataSnapshot pr : sp.child("cart").getChildren()) {
+                                String productKey = pr.getKey();
+                                String qty = "Quantity: "+ pr.getValue().toString();
+                                products.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        String name = "";
+                                        String brand = "";
+                                        String price = "";
+                                        for (DataSnapshot p : snapshot.getChildren()) {
+                                            if (p.getKey().equals(productKey)) {
+                                                name = p.child("name").getValue().toString();
+                                                brand = p.child("brand").getValue().toString();
+                                                price = "$" + Double.parseDouble(p.child("price").getValue().toString());
+                                            }
+                                        }
+                                        addProduct(name, brand, price, qty);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
 
         purchase = findViewById(R.id.purchaseBtn);
         purchase.setOnClickListener(new View.OnClickListener(){
@@ -36,24 +91,28 @@ public class UserCart extends AppCompatActivity {
         });
     }
 
-    private void addProduct(){
-        OrderData userData = new OrderData();
-        Integer i = 0;
-        while(i < userData.productList.size()){
-            final View view = getLayoutInflater().inflate(R.layout.activity_user_product_added_view, null, false);
-            TextView productName = view.findViewById(R.id.item);
-            TextView productQuantity = view.findViewById(R.id.productQuantity);
-            TextView productBrand = view.findViewById(R.id.productBrand);
-            TextView productPrice = view.findViewById(R.id.productPrice);
-            productName.setText(userData.productList.get(i));
-            productQuantity.setText(userData.quantityList.get(i).toString());
-            productBrand.setText(userData.brandList.get(i));
-            productPrice.setText(userData.priceList.get(i).toString());
+    private void addProduct(String product, String brand, String price, String quantity){
+        scrollingLayout = (LinearLayout)findViewById(R.id.userCartScrollLayout);
+        View view = getLayoutInflater().inflate(R.layout.activity_user_product_added_view, null, false);
+        scrollingLayout.addView(view);
 
-            scrollingLayout.addView(view);
-            i++;
-        }
+        TextView itemName = findViewById(R.id.productName);
+        itemName.setId(nameID);
+        TextView itemBrand = findViewById(R.id.productBrand);
+        itemBrand.setId(brandID);
+        TextView itemPrice = findViewById(R.id.productPrice);
+        itemPrice.setId(priceID);
+        TextView itemQuantity = findViewById(R.id.productQuantity);
+        itemQuantity.setId(quantityID);
+
+        itemName.setText(product);
+        itemBrand.setText(brand);
+        itemPrice.setText(price.toString());
+        itemQuantity.setText(quantity.toString());
+
+        nameID++;
+        brandID++;
+        priceID++;
+        quantityID++;
     }
-
-
 }
