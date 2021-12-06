e53c8f135768ce6d5428eaa4924b71a815c95369
Henry-Zhuo <henryzhuo@live.com>
2021-12-06 17:49:47 -0500

    Make logged in user private

--- app/src/main/java/com/example/grocerystoredemoapp/data/LoginRepository.java
+++ app/src/main/java/com/example/grocerystoredemoapp/data/LoginRepository.java
@@ -42,7 +42,7 @@ public class LoginRepository {
 
     // If user credentials will be cached in local storage, it is recommended it be encrypted
     // @see https://developer.android.com/training/articles/keystore
-    static LoggedInUser user = null;
+    private static LoggedInUser user = null;
 
     // private constructor : singleton access
     private LoginRepository(FirebaseAuth mAuth) {
@@ -72,65 +72,5 @@ public class LoginRepository {
         instance.user = user;
         // If user credentials will be cached in local storage, it is recommended it be encrypted
         // @see https://developer.android.com/training/articles/keystore
-    }
-
-
-    public Result<LoggedInUser> register(String username, String password, boolean isAdmin) {
-        // handle login
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-        // Register using Firebase Auth
-        try {
-            // Register using email and password
-            isRegistering.set(true);
-            Task<AuthResult> authResultTask = mAuth.createUserWithEmailAndPassword(username, password)
-                    .addOnCompleteListener(executorService, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            final String registrationContextTag = "Register";
-                            if (task.isSuccessful()) {
-                                // Registration success, set the user with the signed-in user's information
-                                Log.d(registrationContextTag, "registerWithEmail:success");
-                                FirebaseUser firebaseUser = mAuth.getCurrentUser();
-
-                                // Write data from Firebase synchronously
-                                setFirebaseUserData(firebaseUser, username, isAdmin);
-                            } else {
-                                // On registration failure, log it
-                                Log.w(registrationContextTag, "registerWithEmail:failure", task.getException());
-                                //Toast.makeText(LoginActivity.this, "Registration failed.", Toast.LENGTH_SHORT).show();
-                            }
-                            isRegistering.set(false);
-                        }
-                    });
-
-            // Wait until authentication is finished before returning
-            while (isRegistering.get()) {
-                // TODO: Put this in a separate thread and use waits. This infinite while loop is a kludge
-            }
-
-            return new Result.Success<LoggedInUser>(user);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error signing up", e));
-        }
-    }
-
-    private void setFirebaseUserData(FirebaseUser firebaseUser, String email, boolean isAdmin) {
-        final String userId = firebaseUser.getUid();
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference userRef = mDatabase.child("Users").child(userId);
-        // TODO: Fix saving of displayName
-        /*
-        User user = new User(email, isAdmin, "", email);
-
-        userRef.setValue(user)
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("Registration", "Failed to write user data to database", e);
-                    }
-                });
-
-         */
     }
 }
